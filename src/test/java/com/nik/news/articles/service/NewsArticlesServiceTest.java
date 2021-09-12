e7fa315e210178c4d0589cca248243861cfa5e0d
Nikesh Pathak <nikeshpathak@ymail.com>
2021-09-12 22:49:39 +0530

    Initial commit

--- src/test/java/com/nik/news/articles/service/NewsArticlesServiceTest.java
+++ src/test/java/com/nik/news/articles/service/NewsArticlesServiceTest.java
@@ -11,5 +11,69 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+
+import com.nik.news.articles.dto.NewsArticlesDTO;
+import com.nik.news.articles.model.NewsArticles;
+import com.nik.news.articles.repo.NewsArticlesRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class NewsArticlesServiceTest {
+
+    @MockBean
+    NewsArticlesRepo newsArticlesRepo;
+
+    @Autowired
+    NewsArticlesService newsArticlesService;
+
+    @Test
+    public void saveNewsArticlesTest() throws Exception {
+        when(newsArticlesRepo.save(any())).thenReturn(getNewsArticlesObject());
+        newsArticlesService.add(getNewsArticlesObjectDTO());
+        Assert.isTrue(true, "Saved Successfully");
+    }
+
+    @Test
+    public void updateNewsArticlesTest() throws Exception {
+        when(newsArticlesRepo.save(any())).thenReturn(getNewsArticlesObject());
+        when(newsArticlesRepo.findById(any())).thenReturn(Optional.of(getNewsArticlesObject()));
+        newsArticlesService.update(getNewsArticlesObjectDTO());
+    }
+
+    @Test
+    public void getArticlesByIdTest() throws Exception {
+        when(newsArticlesRepo.findById(any())).thenReturn(Optional.of(getNewsArticlesObject()));
+        NewsArticlesDTO newsArticlesDTO = newsArticlesService.get(1);
+        Assert.notNull(newsArticlesDTO, "Data fetch Successfully");
+    }
+
+    private NewsArticles getNewsArticlesObject() {
+        NewsArticles newsArticles = new NewsArticles();
+        newsArticles.setHeader("Mandatory 6th generation middleware");
+        newsArticles.setShortDesc("Poisoning by diagnostic agents, self-harm, sequela");
+        newsArticles.setText("Poisoning by diagnostic agents, intentional self-harm, sequela");
+        newsArticles.setAuthor("Lebbie Withrop");
+        newsArticles.setKeyword("Wrapsafe");
+        return newsArticles;
+    }
+
+    private NewsArticlesDTO getNewsArticlesObjectDTO() {
+        NewsArticlesDTO newsArticles = new NewsArticlesDTO();
+        newsArticles.setHeader("Mandatory 6th generation middleware");
+        newsArticles.setShortDesc("Poisoning by diagnostic agents, self-harm, sequela");
+        newsArticles.setText("Poisoning by diagnostic agents, intentional self-harm, sequela");
+        newsArticles.setAuthor("Lebbie Withrop");
+        newsArticles.setKeyword("Wrapsafe");
+        return newsArticles;
+    }
+
 }
